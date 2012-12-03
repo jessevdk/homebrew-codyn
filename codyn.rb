af92c5a811b14c10e5a415e4cc31e20dd4d67d13
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-12-03 17:53:55 +0100

    Add support for OS X 10.6.8 for codyn
    * By default, install only v3.3 tag
    * Added missing build dependency on automake and libtool

--- codyn.rb
+++ codyn.rb
@@ -3,14 +3,18 @@ require 'formula'
 class Codyn < Formula
   homepage 'http://www.codyn.net'
   head 'git://git.codyn.net/codyn/codyn.git'
-
+  url 'git://git.codyn.net/codyn/codyn.git' , :using => :git, :revision => "c7b7e07f2c084e4770a524effbed4428710182bd"
+  version '3.3'
   option :universal
 
   option 'with-python', 'Enable python support'
 
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
   depends_on 'pkg-config' => :build
   depends_on 'libxml2'
   depends_on 'glib'
+  depends_on 'libtool' => :build
 
   if build.include? 'with-python'
     depends_on 'gobject-introspection'
