8107d0c1361b83c73227fc6ae85b5ecb0a3936d8
Jesse van den Kieboom <jessevdk@gmail.com>
2013-04-30 21:26:48 +0200

    Added formula for pygobject for python 3

---
+++ py3gobject3.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Pygobject3 < Formula
+  homepage 'http://live.gnome.org/PyGObject'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/pygobject/3.8/pygobject-3.8.1.tar.xz'
+  sha256 'e85c8ec64238ce88ec1e905497f5a2eaa947a7f4050e338d21ec510d8a50eacb'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gobject-introspection'
+  depends_on 'glib'
+  depends_on 'cairo'
+  depends_on 'py2cairo'
+
+  option :universal
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-python=python3"
+
+    system "make install"
+  end
+end
